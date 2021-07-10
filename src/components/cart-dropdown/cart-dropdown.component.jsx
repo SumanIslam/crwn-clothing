14e70ef13d7a4eeaec19608a1e25a9d7a9658252
SumanIslam <sumanislam@gmail.com>
2021-07-10 08:19:23 +0600

    created cartItem component and connected cart-dropdown component with cartItems reducer and styled custom scroll-bar in dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,21 @@
-import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
+import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+// eslint-disable-next-line no-unused-vars
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton inverted>GO TO CHECKOUT</CustomButton>
   </div>
 );
-export default CartDropdown;
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+export default connect(mapStateToProps)(CartDropdown);
