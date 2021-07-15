5700ed1a95ae1818293c5bb0bfc5fe68058a2024
SumanIslam <sumanislam@gmail.com>
2021-07-15 06:17:53 +0600

    move default props of directory into redux for testing purpose and added directory reducer

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import CartReducer from './cart/cart.reducer';
 import UserReducer from './user/user.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -13,6 +14,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: UserReducer,
   cart: CartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
