cc569cec50086bc581eeb126e0d21075c67efa41
SumanIslam <sumanislam@gmail.com>
2021-07-17 15:20:03 +0600

    normalizes the data by using collection object instead of collection array

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,20 +1,9 @@
 import { createSelector } from 'reselect';
 
-// eslint-disable-next-line no-unused-vars
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 const selectShopCollections = createSelector([selectShop], (shop) => shop.collections);
 export const selectCollections = (collectionUrlParam) =>
-  createSelector([selectShopCollections], (collections) =>
-    collections.find((collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-  );
+  createSelector([selectShopCollections], (collections) => collections[collectionUrlParam]);
 
 export default selectShopCollections;
