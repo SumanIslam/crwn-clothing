fa53d59ed3a7256317e67de39ce90164b2361e66
SumanIslam <sumanislam@gmail.com>
2021-08-17 02:54:15 +0600

    add useEffect in place of componentDidMount at App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
@@ -10,40 +10,33 @@ import Homepage from './pages/homepage/homepage.components';
 import ShopPageContainer from './pages/shop/shop.container';
 import SignInAndSignOut from './pages/sign-in-and-sign-out-page/sign-in-and-sign-out-page.component';
 import { fetchCollectionsStart } from './redux/shop/shop.actions';
-import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    // eslint-disable-next-line no-shadow
-    const { fetchCollectionsStart, checkCurrentUser } = this.props;
+// eslint-disable-next-line no-shadow
+const App = ({ fetchCollectionsStart, checkCurrentUser, currentUser }) => {
+  useEffect(() => {
     checkCurrentUser();
     fetchCollectionsStart();
-  }
+  }, [checkCurrentUser, fetchCollectionsStart]);
 
-  render() {
-    // eslint-disable-next-line no-shadow
-    const { currentUser } = this.props;
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/shop" component={ShopPageContainer} />
-          <Route exact path="/shop/:collectionId" component={CollectionPageContainer} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() => (currentUser ? <Redirect to="/" /> : <SignInAndSignOut />)}
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPageContainer} />
+        <Route exact path="/shop/:collectionId" component={CollectionPageContainer} />
+        <Route exact path="/checkout" component={CheckoutPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() => (currentUser ? <Redirect to="/" /> : <SignInAndSignOut />)}
+        />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
