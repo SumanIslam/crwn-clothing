142fd31bd3de053b1a28fef826ffe1071511b761
SumanIslam <sumanislam@gmail.com>
2021-10-09 17:04:39 +0600

    implement useMemo hook in cart-item component to boost performance

--- client/src/pages/homepage/homepage.components.jsx
+++ client/src/pages/homepage/homepage.components.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
+
 // components
 import Directory from '../../components/directory/directory.component';
+
 // styles
 import { HomePageContainer } from './homepage.styles';
 
-const Homepage = () => {
-  throw Error;
-  // eslint-disable-next-line no-unreachable
-  return (
-    <HomePageContainer>
-      <Directory />
-    </HomePageContainer>
-  );
-};
+const Homepage = () => (
+  <HomePageContainer>
+    <Directory />
+  </HomePageContainer>
+);
 
 export default Homepage;
