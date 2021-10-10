237ae4b2143e7695568876f88cda8b5147c35b8d
SumanIslam <sumanislam@gmail.com>
2021-10-10 15:46:57 +0600

    update our code to be PWA, service-worker https and update manifest

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -12,7 +13,8 @@ const port = process.env.PORT || 5000;
 
 app.use(compression());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(enforce.HTTPS({trustProtoHeader: true}));
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
 if(process.env.NODE_ENV === 'production') {
@@ -22,6 +24,10 @@ if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   });
 }
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
+})
 
 app.post('/payment', (req, res) => {
   const { token, amount } = req.body;
