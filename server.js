50b41986402ba2f65e11f680d921ce3ce7031c51
SumanIslam <sumanislam@gmail.com>
2021-10-10 16:24:41 +0600

    some changes for pwa

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
+  res.sendFile(path.resolve(__dirname, '..', 'client/build', 'service-worker.js'))
 })
 
 app.post('/payment', (req, res) => {
